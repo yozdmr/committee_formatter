31d641d52f304844a0ef81fd93823b2b3c5f3931
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2023-08-17 18:08:00 -0400

    app.py fix

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from extract import run_doc
 from format import format_doc
 
-# FIX THIS BEFORE COMMIT
-file_name =  'c.docx' # input("Enter the name of your file: ")
+file_name =  input("Enter the name of your file: ")
 
 run_doc(file_name)
 format_doc(file_name)
